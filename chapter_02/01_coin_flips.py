f9812f28d350e2c8065f58f09e69afa0d0bec1aa
yeasar <syeasar.kuet@gmail.com>
2026-03-11 14:13:54 +0600

    new: Add coin filp test

---
+++ chapter_02/01_coin_flips.py
@@ -0,0 +1,13 @@
+import numpy as np
+N = 1000000
+M = 3
+heads = np.zeros(M+1)
+for i in range(N):
+    flips = np.random.randint(0, 2, M)
+    h, _ = np.bincount(flips, minlength=2)
+    heads[h] += 1
+
+prob = heads/N
+
+# print("Probabilities: %s" % np.array2string(prob))
+print(f"Probabilities: {np.array2string(prob)}")
